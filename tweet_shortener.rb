3e3c64d4e75c3de2077b0be03680a4548f927982
Joseph O'Dell <josephodell@outlook.com>
2018-12-17 13:47:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,15 +1,15 @@
 # Write your code here.
 def dictionary
   dictionary_hash = {
-    "hello" => "hi"
-    "to" => "2"
-    "two" => "2"
-    "too" => "2"
-    "for" => "4"
-    "four" => "4"
-    "you" => "u"
-    "at" => "@"
-    "and" => "&"
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
   }
 end
 
